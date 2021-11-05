d9abf6a0c9e748ad541af87384bc5a2de8477915
mozammalrahat <hossain.rahat33@gmail.com>
2021-11-05 10:23:36 +0600

    Initial Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const next = require("next");
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+require("dotenv").config({ path: "./config.env" });
+const connectDb = require("./utilsServer/connectDb");
+connectDb();
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+nextApp.prepare().then(() => {
+  app.use("/api/signup", require("./api/signup"));
+  app.use("/api/auth", require("./api/auth"));
+  app.use("/api/search", require("./api/search"));
+  app.use("/api/posts", require("./api/posts"));
+  app.use("/api/profile", require("./api/profile"));
+  app.use("/api/notifications", require("./api/notifications"));
+
+  app.all("*", (req, res) => handle(req, res));
+
+  server.listen(PORT, err => {
+    if (err) throw err;
+    console.log("Express server running");
+  });
+});
