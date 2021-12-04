3e45fba132fd2546f0b7d98421c63f4d9ee6d156
mozammalrahat <hossain.rahat33@gmail.com>
2021-12-04 16:56:41 +0600

    ResourceHub is working fine -->> One filed "Topic" is added in FileModel.. -->> delete request from frontend is working fine -->> only the user who uploaded the file can delete

--- api/resource/fileList.js
+++ api/resource/fileList.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../../middleware/authMiddleware");
 const FileModel = require("../../models/resource/fileModel");
+const UserModel = require("../../models/UserModel");
 // const uuid = require("uuid").v4;
 
 // CREATE A File
 
 router.post("/", authMiddleware, async (req, res) => {
-  const { fileName, fileType, fileUrl } = req.body;
+  const { fileName, fileType, fileTopic, fileUrl } = req.body;
 
   if (fileName.length < 1)
     return res.status(401).send("File name must be greater than 0");
@@ -18,6 +19,7 @@ router.post("/", authMiddleware, async (req, res) => {
       fileName,
       fileUrl,
       fileType,
+      fileTopic,
     };
     const file = await new FileModel(newFile).save();
 
@@ -50,6 +52,38 @@ router.get("/", authMiddleware, async (req, res) => {
       ]);
 
     return res.json(FilesToBeSent);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(`Server error`);
+  }
+});
+
+// DELETE POST
+
+router.delete("/:fileId", authMiddleware, async (req, res) => {
+  try {
+    const { userId } = req;
+
+    const { fileId } = req.params;
+
+    const file = await FileModel.findById(fileId);
+    if (!file) {
+      return res.status(404).send("File not found");
+    }
+
+    const user = await UserModel.findById(userId);
+
+    if (file.user.toString() !== userId) {
+      if (user.role === "root") {
+        await file.remove();
+        return res.status(200).send("File deleted Successfully");
+      } else {
+        return res.status(401).send("Unauthorized");
+      }
+    }
+
+    await file.remove();
+    return res.status(200).send("file deleted Successfully");
   } catch (error) {
     console.error(error);
     return res.status(500).send(`Server error`);
