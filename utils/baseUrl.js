a48004d37c3b602ad2d72bb9fcde64eb19413bdb
Mozammal Hossain <40220345+mozammalrahat@users.noreply.github.com>
2023-05-13 18:23:32 +0600

    Update baseUrl.js for base URL

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://focu-s.herokuapp.com";
+    : "https://645f7da50aa7bd375fdd15b2--grand-brigadeiros-3c0359.netlify.app/";
 
 module.exports = baseUrl;
