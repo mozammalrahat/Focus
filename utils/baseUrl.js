e1827f8fd98005d372d213a71668da2e6bef0908
Mozammal Hossain <40220345+mozammalrahat@users.noreply.github.com>
2023-05-14 13:43:35 +0600

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "http://localhost:3000";
+    : "https://grand-brigadeiros-3c0359.netlify.app/";
 
 module.exports = baseUrl;
